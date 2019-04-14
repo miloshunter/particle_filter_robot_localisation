d4a97fa05f6a149cbcaff6f5a1f5354e944fa55a
Milos Arbanas <milostravian2@gmail.com>
2019-04-14 16:59:16 +0200

    Tried to optimize by paralelizing, but did not succeed. Probably gonna rewrite in C or C++.

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from Lavirint import Lavirint
 from Robot import Robot
 import threading
 from Partikl import Partikl
+import time
+from joblib import Parallel, delayed
 
+
+do_parallel = False
 
 class Simulacija:
     def __init__(self):
@@ -41,7 +45,7 @@ class Simulacija:
         # Simulira dok se self.simuliraj ne postavi na False
         self.simuliraj = True
         while self.simuliraj:
-            self.dt = self.clock.tick(parametri.FPS) / 1000.0
+            #self.dt = self.clock.tick(parametri.FPS) / 1000.0
             self.dogadjaji()
             self.crtaj()
             self.azuriraj()
@@ -52,6 +56,9 @@ class Simulacija:
         sys.exit()
 
     def algoritam(self):
+
+        start_time = time.time()
+
         partikli = simulacija.partikli
 
         izmerio_robot = simulacija.robot.laser.merenje_lasera
@@ -69,15 +76,19 @@ class Simulacija:
         if len(partikli) > 0:
             while len(partikli) < parametri.BROJ_PARTIKALA:
                 indeks = random.randrange(len(partikli))
-                distx = 100
-                disty = 100
-                rot = 45
+                distx = 50
+                disty = 50
+                rot = 15
                 self.kreiraj_partikl((int(partikli[indeks].pos[0]-distx), int(partikli[indeks].pos[0]+disty)),
                                      (int(partikli[indeks].pos[1] - distx), int(partikli[indeks].pos[1] + disty)),
                                      (int(partikli[indeks].rot-rot), int(partikli[indeks].rot+rot)))
         else:
             while len(self.partikli) < parametri.BROJ_PARTIKALA:
                 self.kreiraj_partikl((10, 768), (10, 768), (0, 360))
+
+        end_time = time.time()
+
+        print("Algorithm time = ", end_time - start_time)
 
     def dogadjaji(self):
         for dogadjaj in pg.event.get():
@@ -95,17 +106,41 @@ class Simulacija:
                         partikl.crtaj_partikl = not partikl.crtaj_partikl
 
     def azuriraj(self):
-        self.svi_sprajtovi.update()
+        start_time = time.time()
+        if do_parallel:
+            def par_update(sprajt):
+                sprajt.update()
+
+            Parallel(n_jobs=-1, backend="threading")(
+                map(delayed(par_update), self.svi_sprajtovi)
+            )
+        else:
+            self.svi_sprajtovi.update()
+        end_time = time.time()
+        print("Updating time = ", end_time - start_time)
 
 
 
     def crtaj(self):
         # Iscrtavanje sa dvostrukim baferovanjem
+        start_time = time.time()
         self.ekran.fill(parametri.BOJA_POZADINE)
-        for sprajt in self.svi_sprajtovi:
-            self.ekran.blit(sprajt.image, sprajt.rect)
 
-        pg.display.flip()
+        if do_parallel:
+            def par_draw(sprajt):
+                self.ekran.blit(sprajt.image, sprajt.rect)
+
+            Parallel(n_jobs=-1, backend="threading")(
+                map(delayed(par_draw), self.svi_sprajtovi)
+            )
+        else:
+            for sprajt in self.svi_sprajtovi:
+                self.ekran.blit(sprajt.image, sprajt.rect)
+
+        pg.display.update()
+
+        end_time = time.time()
+        print("Drawing time = ", end_time - start_time)
 
 
 simulacija = Simulacija()
@@ -116,7 +151,7 @@ def printit():
     simulacija.tajmer_ispisa = tajmer_ispisa
     tajmer_ispisa.setDaemon(False)
     tajmer_ispisa.start()
-    print("Robot meri: " + str(simulacija.robot.laser.merenje_lasera))
+    #print("Robot meri: " + str(simulacija.robot.laser.merenje_lasera))
 
 
 try:
